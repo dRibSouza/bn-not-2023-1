47ed93d6e78e1f1c06b863955b56be7ea7b92020
dRibSouza <92061792+dRibSouza@users.noreply.github.com>
2023-05-16 00:49:18 +0000

    adição de filtros por query

--- back-end/controllers/sale.js
+++ back-end/controllers/sale.js
@@ -1,5 +1,8 @@
 // Importação do model
 const Sale = require('../models/Sale')
+const qpm = require('query-params-mongo')
+
+const processQuery = qpm()
 
 const controller = {}   // Objeto vazio
 
@@ -21,8 +24,21 @@ controller.create = async (req, res) => {
 
 controller.retrieveAll = async (req, res) => {
   try {
+
+    let filter = {}
+
+    // Se tiverem sido passados parâmetros de busca
+    if(Object.keys(req.query).length > 0) {   // Se foram passados
+      // Retorna todos os documentos da coleção
+      const query = processQuery(req.query, {}, false)
+      filter = query.filter
+    }
+
     // Retorna todos os documentos da coleção
-    const result = await Sale.find().populate('customer')
+    const result = await Sale.find(filter)
+      .populate('customer')
+      .populate('items.product')
+
     // HTTP 200: OK (implícito)
     res.send(result)
   }
@@ -35,7 +51,9 @@ controller.retrieveAll = async (req, res) => {
 
 controller.retrieveOne = async(req, res) => {
   try {
-    const result = await Sale.findById(req.params.id).populate('customer')
+    const result = await Sale.findById(req.params.id)
+      .populate('customer')
+      .populate('items.product')
 
     if(result) {
       // Encontrou o documento ~> HTTP 200: OK (implícito)
@@ -56,7 +74,50 @@ controller.retrieveOne = async(req, res) => {
 controller.update = async (req, res) => {
   try {
 
-    const result = await Sale.findByIdAndUpdate(req.params.id, req.body)
+    // Encontra a venda para ser atualizada
+    const sale = await Sale.findById(req.params.id)
+    
+    // items foi passado em req.body
+    if(req.body.items) {
+      // Percorre cada item de req.body, verificando se já existe
+      // ou não em sales.item
+      for(let item of req.body.items) {
+        // Se o item tem _id, é porque já existe ~> É CASO DE ATUALIZAÇÃO
+        if(item._id) {
+
+          // Verifica se foi passada uma propriedade especial, chamada
+          // '$_delete', com o valor true e, nesse caso, deleta o subdocumento
+          if(item['$_delete'] === true) {
+            sale.items.id(item._id).deleteOne()
+          }
+          else {
+            // Procura cada propriedade no item de req.body e atualiza no documento
+            for(let prop in item) {
+              sale.items.id(item._id)[prop] = item[prop]
+            }
+          }
+        }
+        // Item não existe ~> É caso de inserção
+        else {
+          sale.items.push(item)   // Cria um novo item
+        }
+      }
+
+      // Indica que o items foi modificado e deve ser regravado
+      sale.markModified('items')
+
+    }
+
+    // Verifica as demais propriedades do pai (sale) por alterações
+    for(let prop in req.body) {
+      if(prop !== 'items') {  // Items já foi processado acima
+        console.log({prop})
+        sale[prop] = req.body[prop]
+        sale.markModified(prop)
+      }
+    }
+    
+    const result = await sale.save()
 
     if(result) {
       // Encontrou e atualizou ~> HTTP 204: No content
